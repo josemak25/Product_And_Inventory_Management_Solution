3405addfd3a68cb83c90b336c6ede7cb58e5c607
josemak25 <amakirij@gmail.com>
2019-02-28 09:31:10 +0100

    Added update method for a product

--- js/inventory.js
+++ js/inventory.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   const $tr = $("#tablepopulate");
   const formName = $("#name");
-  const formQuantity = $("#name");
+  const formQuantity = $("#prodQaun");
   const formPrice = $("#price");
   const formCategory = $("#catId");
   function populate(product) {
     console.log(product.name);
     $tr.append(`
-      <td>${product.name}</td>
+      <tr> <td>${product.name}</td>
       <td>${product.price}</td>
       <td>${product.category}</td>
-      <td>${product.quantity}</td>
+      <td>${product.qauntity}</td>
       <td>
         <a id="editButton" data-toggle="modal" data-target="#exampleModal"
           >edit</a
@@ -18,7 +18,8 @@ $(document).ready(function() {
       </td>
       <td>
         <a id="deleteButton">remove</a>
-      </td>`);
+      </td>
+      </tr>`);
   }
 
   $.ajax({
@@ -29,5 +30,36 @@ $(document).ready(function() {
         populate(product);
       });
     }
+  });
+  $("#newProduct").on("click", function(e) {
+    e.preventDefault();
+    const FormData = {
+      name: formName.val(),
+      description: formDescription.val(),
+      qauntity: formQuantity.val(),
+      imageLink: formImageLink.val(),
+      price: formPrice.val(),
+      category: formCategory.val()
+    };
+    $("#newProduct").trigger("reset");
+
+    $.ajax({
+      type: "PUT",
+      url: "http://localhost:3000/products",
+      data: FormData,
+      success: () => {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000
+        });
+
+        Toast.fire({
+          type: "success",
+          title: "Updated product successfully"
+        });
+      }
+    });
   });
 });
