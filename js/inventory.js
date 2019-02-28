fb3cdf970a9e4fd8c4747773d675c26e7ad026c9
josemak25 <amakirij@gmail.com>
2019-02-28 08:13:48 +0100

    Added a populate function to pupolate data to table

--- js/inventory.js
+++ js/inventory.js
@@ -4,4 +4,19 @@ $(document).ready(function() {
   const formQuantity = $("#name");
   const formPrice = $("#price");
   const formCategory = $("#catId");
+  function populate(product) {
+    $tr.append(`
+      <td>${formName}</td>
+      <td>${formPrice}</td>
+      <td>${formCategory}</td>
+      <td>${formQuantity}</td>
+      <td>
+        <a id="editButton" data-toggle="modal" data-target="#exampleModal"
+          >edit</a
+        >
+      </td>
+      <td>
+        <a id="deleteButton">remove</a>
+      </td>`);
+  }
 });
