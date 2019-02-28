ecbe8dcd046781c1c30dfab92f5af7c78d55c198
josemak25 <amakirij@gmail.com>
2019-02-28 08:12:49 +0100

    Added a function for inventory table

--- js/inventory.js
+++ js/inventory.js
@@ -1,1 +1,7 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  const $tr = $("tr");
+  const formName = $("#name");
+  const formQuantity = $("#name");
+  const formPrice = $("#price");
+  const formCategory = $("#catId");
+});
