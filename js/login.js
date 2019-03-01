8bc29529b557928f6c0ff6e43f37e9730c96231a
josemak25 <amakirij@gmail.com>
2019-03-01 07:44:38 +0100

    Added fix for new products

--- js/login.js
+++ js/login.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $("#changeFirst").click(function(e) {
     e.preventDefault();
-    $("#changeFirst").text("Sign Up");
-    $("#changeSecond").text("Sign Up");
+    let 
   });
 });
